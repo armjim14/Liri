91aa1f2526f20843842fec93383ff213ddcd34f8
armjim14 <ajimenez1497@gmail.com>
2019-07-07 17:40:23 -0700

    corrected fiels

--- liri.js
+++ liri.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
 var axios = require("axios");
 var Spotify = require('node-spotify-api');
 var fs = require("fs");
+var os = require("os");
+var moment = require("moment");
+var keys = require("./keys.js");
 
 var Where = process.argv[2];
 var info = process.argv.slice(3).join(" ");
@@ -58,7 +62,7 @@ function getconcert(){
         for ( e in all ){
             console.log(`Name of location: ${all[e].venue.name}`);
             console.log(`Where: ${all[e].venue.city}, ${all[e].venue.region}, ${all[e].venue.country}`);
-            console.log(`When: ${all[e].datetime}`)
+            console.log(`When: ${moment(all[e].datetime).format("MM-DD-YYYY")}`)
             console.log("");
         }
 
@@ -68,10 +72,8 @@ function getconcert(){
 }
 
 function getsong() {
-    var spotify = new Spotify({
-        id: "aae181ff8f3b4139867cdd9c3809b2c8",
-        secret: "b2f2b0943d224c948e42edcdf89da630"
-      });
+
+    var spotify = new Spotify(keys.spotify);
 
     spotify.search({type: "track", query: info}, (err, data) => {
         if (err){
@@ -86,35 +88,27 @@ function getsong() {
         }
 
         for (e in better){
-<<<<<<< HEAD
-        console.log(`Artist Name: ${better[e].album.artists[0].name}`);
-        console.log(`Album Name: ${better[e].album.name}`);
-        console.log(`Sporify URL: ${better[e].album.external_urls.spotify}`);
-        console.log(`Released date: ${better[e].album.release_date}`);
-        console.log("");
-=======
             var one = `Artist Name: ${better[e].album.artists[0].name}`;
             var two = `Album Name: ${better[e].album.name}`;
-            var three = `Released date: ${better[e].album.release_date}`;    
+            var three = `Released date: ${moment(better[e].album.release_date).format("MM-DD-YYYY")}`;    
             console.log(`Artist Name: ${better[e].album.artists[0].name}`);
             console.log(`Album Name: ${better[e].album.name}`);
-            console.log(`Released date: ${better[e].album.release_date}`);
+            console.log(`Released date: ${moment(better[e].album.release_date).format("MM-DD-YYYY")}`);
             console.log("");
             var sendToFile = {
                 one: one,
                 two: two,
                 three, three
             }
->>>>>>> c3c78957a6b27194b255af44cde87ed52d138764
         }
 
-        fs.appendFile("spotifyInfo.txt", JSON.stringify(sendToFile), (err, data) => {
+        fs.appendFile("spotifyInfo.txt", JSON.stringify(sendToFile)+os.EOL, (err) => {
 
             if (err){
                 return console.log(err);
             }
 
-            console.log(`${data} send to spotifyInfo.txt`);
+            console.log(`first response got send to spotifyInfo.txt`);
         })
 
     })
